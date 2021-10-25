0f7879f7bbc8907cdb9cd37707e0c1e538c1fb82
eberhardimbarte <eberhardimbarte@mail.de>
2021-10-25 21:19:26 +0200

    Add documentation for __main__

--- src/refmgr/__main__.py
+++ src/refmgr/__main__.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+"""The __main__ module lets you run the CLI interface."""
+
 
 from . import main
 
