c3b9d9abe34e7b9ed923ac7c35889009ccac21b9
eberhardimbarte <eberhardimbarte@mail.de>
2021-11-10 18:29:56 +0100

    Implement importing of bib file

--- src/refmgr/importrefs.py
+++ src/refmgr/importrefs.py
@@ -14,13 +14,46 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
 """Import references."""
 
+import logging
+import os.path
+import warnings
+
 from . import conf
+from . import bibtex
 
 
 def importrefs(args):
     """Import the given references."""
-    refs = args.refs
-    print(f'Importing references not possible (feature not yet implemented)')
+    for ref in args.refs:
+        importbib(ref)
+
+
+def newbibpath(path):
+    """Make the new path for the BibTeX file."""
+    basename = os.path.basename(path)
+    dirname = os.path.realpath(
+        os.path.normpath(
+            os.path.expanduser(
+                conf['library']['path'])))
+    return os.path.join(dirname, basename)
+
+
+def importbib(path):
+    """Import the bibtex file at the given path."""
+    bparser = bibtex.initparser()
+    bwriter = bibtex.initwriter()
+
+    with open(path, 'r') as infile:
+        db = bparser.parse_file(infile)
+
+    outpath = newbibpath(path)
+    try:
+        with open(outpath, 'x') as outfile:
+            outfile.write(bwriter.write(db))
+            msg = f"imported '{path}' to '{outpath}'"
+            logging.info(msg)
+    except FileExistsError:
+        msg = f"cannot import '{path}': '{outpath}' already exists"
+        warnings.warn(msg, RuntimeWarning)
